0bdee237bfdbc9850ee6c4dfaf902ef8ec204e63
DEBARSI MONDAL <141905446+mDebarsi@users.noreply.github.com>
2023-12-08 20:29:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function fibonacci(num) {
 // your code here
+	  if (num === 0) return 0;
+  if (num === 1) return 1;
+
+  let prev = 0;
+  let current = 1;
+
+  for (let i = 2; i <= num; i++) {
+    const temp = current;
+    current += prev;
+    prev = temp;
+  }
+
+  return prev;
 }
 
 module.exports = fibonacci;
